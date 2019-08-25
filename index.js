3a0ef62ecf771e6d0d72d31a5bee2e434b28c1ab
bwlight <bwlight1@gmail.com>
2019-08-24 21:51:14 -0500

    Fu?

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ const client = new Discord.Client();
 const settings = require('./settings.json');
 
 client.once("ready", () => {
-  console.log("I am ready!");
+  console.log("I am ready to Dominate!");
 });
 
 var prefix = "~"
-client.on("message", message => {
+client.on('message', message => {
   if (message.author === client.user) return;
-  if (message.content.startwith === (prefix + "ping")) {
-    message.channel.sendMessage("pong!");
-  }
+  if (message.content.startsWith(prefix + 'foo')) {
+		message.channel.sendMessage('bar');
+	}
 });
 
 client.login(settings.token);
