56b2e922936f470d7336f8e4d31eeba12784829e
bwlight <bwlight1@gmail.com>
2019-08-24 19:19:31 -0500

    inital

---
+++ index.js
@@ -0,0 +1,8 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.login("zQ1.XWHRBQ.QPA5YgxKrhGYf-6eNZsM0EFybgQ");
