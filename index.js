7c9433ce034b1cb27a166d3be64ac136cedf27f3
bwlight <bwlight1@gmail.com>
2019-08-24 21:42:17 -0500

    IT"S UP!!

--- index.js
+++ index.js
@@ -4,11 +4,12 @@ const settings = require('./settings.json');
 
 client.once("ready", () => {
   console.log("I am ready!");
-})
+});
+
 var prefix = "~"
 client.on("message", (message) => {
   if (message.author === client.user) return;
-  if (message.content.startwith === (prefix + "!ping")) {
+  if (message.content.startwith === (prefix + "ping")) {
     message.channel.sendMessage("pong!");
   }
 });
