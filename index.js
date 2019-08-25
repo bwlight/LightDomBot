0a94d42e4aa5f255735e414d6f66aecc902d4921
bwlight <bwlight1@gmail.com>
2019-08-24 21:57:31 -0500

    fix error

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 const settings = require('./settings.json');
+const prefix = "~"
 
 client.once("ready", () => {
   console.log("I am ready to Dominate!");
 });
 
-var prefix = "~"
+
 client.on('message', message => {
   if (message.author === client.user) return;
   if (message.content.startsWith(prefix + "ping")) {
